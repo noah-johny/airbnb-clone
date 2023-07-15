9182c904a08f6303aae5d54e864d7a07cd0a75e7
Noah <noahjoh11@gmail.com>
2023-07-15 18:11:30 +0530

    Added dropdown to User Menu

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,9 +1,18 @@
 "use client";
 
+import { useCallback, useState } from "react";
+
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
+import MenuItem from "./MenuItem";
 
 function UserMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, []);
+
   return (
     <div className="relative">
       <div className="flex flex-row items-center gap-2">
@@ -13,13 +22,29 @@ function UserMenu() {
         </div>
 
         {/* User Menu */}
-        <div className="flex flex-row gap-3 p-2 items-center border-[1px] border-neutral-200 rounded-full cursor-pointer hover:shadow-md transition">
+        <div
+          onClick={toggleOpen}
+          className="flex flex-row gap-3 p-2 items-center border-[1px] border-neutral-200 rounded-full cursor-pointer hover:shadow-md transition"
+        >
           <AiOutlineMenu />
           <div className="hidden md:flex">
             <Avatar />
           </div>
         </div>
       </div>
+
+      {/* User Menu (Dropdown) */}
+      {isOpen && (
+        <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm"></div>
+      )}
+      {isOpen && (
+        <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-14 text-sm">
+          <div className="flex flex-col">
+            <MenuItem onClick={() => {}} label={"Login"} />
+            <MenuItem onClick={() => {}} label={"Sign Up"} />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
